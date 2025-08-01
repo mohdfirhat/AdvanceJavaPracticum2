db0ef5b2504de5675049051a02d49daaecd5c369
firhat <mohd.firhat@hotmail.com>
2025-08-01 16:27:24 +0800

    added Reciever class and dataStore.txt

--- src/Receiver.java
+++ src/Receiver.java
@@ -1,5 +1,37 @@
-public class Receiver {
-    public void method1(){
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Receiver {
+
+    private static final String FILE_PATH = "./src/dataStore.txt";
+    private final ArrayList<String> list = new ArrayList<>();
+
+    public Receiver() {
+    }
+
+    public void readFile() {
+
+        try (
+                FileReader fr = new FileReader(FILE_PATH);
+                BufferedReader br = new BufferedReader(fr)) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+
+        } catch (IOException fnf) {
+            //TODO: May need to handle IOException and FileNotFoundException individually
+            System.err.println("Unable to read file.File not found.");
+            return;
+        }
+
+    }
+
+    public ArrayList<String> getList() {
+        return list;
     }
 }
