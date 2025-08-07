57b50f046aff09cff692b8957b0e46b3e72fff1e
firhat <mohd.firhat@hotmail.com>
2025-08-07 16:57:02 +0800

    added java docs

--- src/Invoker/Invoker.java
+++ src/Invoker/Invoker.java
@@ -4,13 +4,24 @@ import java.util.Stack;
 import Command.Command;
 import Exception.InvalidInputException;
 
+/**
+ * Invoker Class is used to run the array of {@link Command}(s) given from the {@code Client}
+ * It adds the commands that is successfully run and adds to the {@code history} stack if it is undoable.
+ */
 public class Invoker {
+    /** variable for an array of {@link Command} for the invoker to execute*/
     private Command[] cmdToExecute;
 
+    /** setter to set the array of {@link Command} {@code cmdToExecute} for the invoker to execute*/
     public void setCommandsForExecution(Command[] cmdToExecute) {
         this.cmdToExecute = cmdToExecute;
     }
 
+    /**
+     * run the array of {@link Command}(s) given from the {@code Client}
+     * It adds the commands that is successfully run and adds to the {@code history} stack if it is undoable.
+     * @param history a Stack of Commands which stores the previous successful undoable commands
+     */
     public void executeCommand(Stack<Command> history){
         for (Command command : cmdToExecute) {
             try {
--- src/Receiver/Receiver.java
+++ src/Receiver/Receiver.java
@@ -3,7 +3,6 @@ package Receiver;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import Exception.InvalidInputException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,20 +10,37 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import Command.Command;
+
+/**
+ * Reciever Class is able to read and store data into the {@code dataStore.txt}.
+ * It also has a {@code list} which temporarily stores the data for manipulation
+ * Receiver Class executes the methods that the {@link Command} calls for and
+ * manipulate the {@code list}.
+ */
 public class Receiver {
 
+    /** static variable to store the dataStore file path*/
     private static final String FILE_PATH = "./src/dataStore.txt";
+    /** variable to temporarily hold the dataStore input for manipulation*/
     public final ArrayList<String> list = new ArrayList<>();
 
+    /** Constructor for {@link Receiver}. runs the {@code readFile} method
+     * when created*/
     public Receiver() {
         this.readFile();
     }
 
+    /**
+     * reads the dataStore.txt and add the formatted results into the {@code
+     * list}. It creates the dataStore file if it doesn't exist(at the {@code
+     * FILE_PATH}).
+     */
     public void readFile() {
         Path filepath = Paths.get(FILE_PATH);
         String line;
 
-        if  (Files.notExists(filepath)) {
+        if (Files.notExists(filepath)) {
             try {
                 Files.createFile(filepath);
             } catch (IOException io) {
@@ -37,7 +53,7 @@ public class Receiver {
             }
         }
 
-        try ( BufferedReader br = Files.newBufferedReader(filepath) ) {
+        try (BufferedReader br = Files.newBufferedReader(filepath)) {
             while ((line = br.readLine()) != null) {
                 line = line.trim().replaceAll("^[0-9]+[.]\\s", "").replaceAll("\\s", " ");
                 list.add(line);
@@ -51,13 +67,16 @@ public class Receiver {
 
     }
 
+    /**
+     * Stores the list into the dataStore.txt
+     */
     public void storeToFile() {
         Path filepath = Paths.get(FILE_PATH);
         String line = "";
 
         try (BufferedWriter buff_writer = Files.newBufferedWriter(filepath)) {
-            for (int i=0; i<list.size(); i++) {
-                buff_writer.write(String.format("%02d. %s",i+1,list.get(i)));
+            for (int i = 0; i < list.size(); i++) {
+                buff_writer.write(String.format("%02d. %s", i + 1, list.get(i)));
                 buff_writer.newLine();
             }
         } catch (IOException io) {
@@ -72,31 +91,52 @@ public class Receiver {
         }
     }
 
+    /** getter for the list */
     public ArrayList<String> getList() {
         return list;
     }
 
-    public void add(String entry){
+    /**
+     * Adds the successful entry to the end of the list.
+     * @param entry input provided by user
+     */
+    public void add(String entry) {
         list.add(entry);
     }
 
-    public void add(int index, String entry){
-        list.add(index,entry);
+    /**
+     * Adds the successful entry to the list at the index provided.
+     * @param index index to add entry
+     * @param entry input provided by user
+     */
+    public void add(int index, String entry) {
+        list.add(index, entry);
     }
 
-
-    public void delete(int index){
+    /**
+     * Remove the entry from the list from the list
+     * @param index to remove entry
+     */
+    public void delete(int index) {
         list.remove(index);
     }
 
-    public void list(){
+    /**
+     * displays the list of successful entries
+     */
+    public void list() {
         for (int i = 0; i < list.size(); i++) {
-            int count = i+1;
+            int count = i + 1;
             System.out.println(String.format("%02d. %s", count, list.get(i)));
         }
     }
 
-    public void update(int index, String entry){
+    /**
+     * updates the list based on the index listed on the list displayed
+     * @param index to update entry
+     * @param entry input provided by user
+     */
+    public void update(int index, String entry) {
         list.set(index, entry);
     }
 
@@ -118,26 +158,26 @@ public class Receiver {
 //        return String.format("%s %s %s", titleCase(inputArr[0]), titleCase(inputArr[1]),email);
 //    }
 
-    private boolean isInvalidData3(String email) {
-        Pattern pattern = Pattern.compile("(^(?![.-])" +
-                "[a-zA-Z0-9_]+(?:[.-](?![.-])[a-zA-Z0-9_]+)*" +
-                "(?<![.-])@" +
-                "(?![.-])" +
-                "[a-zA-Z0-9]+(?:[." +
-                "-](?![.-])[a-zA-Z0-9]+)*" +
-                "(?<![.-])" +
-                "\\.([a-z]{2,3})$|" +
-                "^[A-Za-z0-9_]+)");
-        Matcher matcher = pattern.matcher(email);
-        return !matcher.find();
-    }
+//    private boolean isInvalidData3(String email) {
+//        Pattern pattern = Pattern.compile("(^(?![.-])" +
+//                "[a-zA-Z0-9_]+(?:[.-](?![.-])[a-zA-Z0-9_]+)*" +
+//                "(?<![.-])@" +
+//                "(?![.-])" +
+//                "[a-zA-Z0-9]+(?:[." +
+//                "-](?![.-])[a-zA-Z0-9]+)*" +
+//                "(?<![.-])" +
+//                "\\.([a-z]{2,3})$|" +
+//                "^[A-Za-z0-9_]+)");
+//        Matcher matcher = pattern.matcher(email);
+//        return !matcher.find();
+//    }
 
-    private String titleCase(String data) {
-        data = data.toLowerCase();
-        String firstWord = data.substring(0, 1).toUpperCase();
-        String restOfWord = data.substring(1);
-        return firstWord + restOfWord;
-
-    }
+//    private String titleCase(String data) {
+//        data = data.toLowerCase();
+//        String firstWord = data.substring(0, 1).toUpperCase();
+//        String restOfWord = data.substring(1);
+//        return firstWord + restOfWord;
+//
+//    }
 
 }
